13ac8cab9158b25eb51d7e7540b2b9c50d5ba9d5
Abubakkar2468 <asrtthananahu@gmail.com>
2020-10-18 09:55:59 +0530

    remove lazysize

--- src/app/dashboard/component/spacex-landing/spacex-landing.component.ts
+++ src/app/dashboard/component/spacex-landing/spacex-landing.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DashboardService } from '../../service/dashboard.service';
 import { Router, ActivatedRoute } from '@angular/router';
-import 'lazysizes';
 
 @Component({
   selector: 'app-spacex-landing',
