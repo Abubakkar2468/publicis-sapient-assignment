ea8e77f4a11a4f61fe3eb5f816ca420a5c4c149d
Abubakkar2468 <asrtthananahu@gmail.com>
2020-10-18 13:48:49 +0530

    duplicate module removed

--- src/app/dashboard/service/dashboard.service.ts
+++ src/app/dashboard/service/dashboard.service.ts
@@ -1,11 +1,14 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DashboardService {
+
+  private spacexApi = environment.API_URL;
 
   constructor(
     private http: HttpClient
@@ -19,7 +22,7 @@ export class DashboardService {
     `&launch_success=${(obj.successfullLaunch && obj.successfullLaunch === 'True') ? true : false }` : '';
     params += (obj.successfullLanding) ?
     `&land_success=${(obj.successfullLanding && obj.successfullLanding === 'True') ? true : false }` : '';
-    const url = `https://api.spacexdata.com/v3/launches?limit=100${params}`;
+    const url = `${this.spacexApi}/launches?limit=100${params}`;
     return this.http.get(url);
   }
 
